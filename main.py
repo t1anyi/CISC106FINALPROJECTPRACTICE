f4dc9705fb46e49050be3ae27cd88f5f29ecfce4
James Wu <114433971+t1anyi@users.noreply.github.com>
2026-09-12 13:47:25 -0400

    Implement banana counting features and update title
    Added functionality to increment, decrement, and reset a count of bananas. Updated website title and state initialization.

--- main.py
+++ main.py
@@ -1,23 +1,54 @@
-from bakery import assert_equal
 from drafter import *
 from dataclasses import dataclass
 
 
-
-hide_debug_information()
-set_website_title("Your Drafter Website")
-set_website_framed(False)
-
-
 @dataclass
 class State:
-    pass
+    count: int
 
 
 @route
 def index(state: State) -> Page:
-    return Page(state, ["Hello World!"])
+    if state.count < 0:
+        state.count = 0
+    return Page(state, [
+        Image(url='bana.jpg', width=None, height=None),
+        "Bananas: " + str(state.count) + "\n",
+        Button("+1", "increment"),
+        Button("-1", "decrement"),
+        Button("+5", "add_five"),
+        Button("Reset", "reset_count")
+    ])
 
-assert_equal(index(State()), Page(State(), ["Hello World!"]))
+@route
+def increment(state: State) -> Page:
+    state.count = state.count + 1
+    return index(state)
+@route
+def decrement(state: State) -> Page:
+    state.count = state.count - 1
+    return index(state)
+@route
+def reset_count(state: State) -> Page:
+    state.count = 0
+    return index(state)
+@route
+def add_five(state:State) -> Page:
+    state.count = state.count + 5
+    return (index(state))
+
+
+set_website_title("Counting Bananas")
+set_site_information(
+    author="James Wu",
+    description="",
+    sources="",
+    planning="",
+    links=[]
+)
+hide_debug_information()
+set_website_framed(False)
+start_server(State(5))
+
 
 start_server(State())
